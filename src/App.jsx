d7682c491ceeada1493e501a9290cfee669ad946
OdashevBexruzbek <bexruzbekodashev@gmail.com>
2024-06-26 11:14:49 +0500

    todo app

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,23 @@ import toast from 'react-hot-toast'
 //redux
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
-import { addTodo } from './todoSlice'
+import { addTodo, removeTodo, changeStatusTodo } from './todoSlice'
 
 function App() {
   const dispatch = useDispatch()
   const inputText = useRef()
 
-  const {todos} = useSelector((state) => state.todos)
-  
-  
+  const {todos, completedCount, unCompletedCount} = useSelector((state) => state.todos)
 
   const handeSubmit = (e) =>{
     e.preventDefault();
-    const InputValue = inputText.current.value.trim;
+    const InputValue = inputText.current.value.trim();
     if (InputValue) {
-      dispatch(addTodo(InputValue));
+      dispatch(addTodo({
+        id: Math.floor(Math.random() * 100),
+        text: InputValue,
+        completed: false,
+      }));
       toast.success("Added succes")
     } else {
       alert("Error")
@@ -31,6 +33,8 @@ function App() {
 
   return (
     <>
+    <div>
+    {/* <video className='bg-video' src="./todo-bg.mp4" autoPlay muted loop></video> */}
       <h1>Todo List - {todos.length}</h1>
       <form onSubmit={handeSubmit}>
         <div className='form-input'>
@@ -39,6 +43,26 @@ function App() {
         </div>
         <button>Add</button>
       </form>
+
+      {todos.map((todos) =>{
+        return (
+          <div key={todos.id} className={`todo-form ${todos.completed ? 'completed' : "" }`}>
+            <h4>{todos.text}</h4>
+            <div className='check-btn'>
+              <input type="checkbox" onClick={() => dispatch(changeStatusTodo(todos.id))}
+              checked={todos.completed} readOnly/>
+              <button onClick={() =>dispatch(removeTodo(todos.id))}>Deleted</button>
+            </div>
+          </div>
+        )
+      })}
+
+      <div>
+        <h4>Completed: {completedCount}</h4>
+        <h4>UnCompleted: {unCompletedCount}</h4>
+      </div>
+    </div>
+    
     </>
   )
 }
--- src/todoSlice.js
+++ src/todoSlice.js
@@ -1,32 +1,56 @@
 import { createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
 
 const initialState = {
-    todos: [],
-}
+  todos: [],
+};
 
-const initialStateFromLocal = () =>{
-    return (
-        JSON.parse(localStorage.getItem("todos")) || {
-            
-        }
-    )
-}
+const initialStateFromLocal = () => {
+  return (
+    JSON.parse(localStorage.getItem("todos")) || {
+      todos: [],
+      completedCount: 0,
+      unCompletedCount: 0,
+    }
+  );
+};
 
 const todosSlice = createSlice({
-    name: "Todos",
-    initialState,
-    reducers: {
-        addTodo: (state, {payload}) =>{
-            console.log(payload);
-            // state.todos = [...state.todos, payload]
-        },
-        removeTodo: (state, {payload}) =>{
-            state.todos
-        },
-        changeStatusTodo: (state, {payload}) =>{}
-    }
-})
+  name: "Todos",
+  initialState: initialStateFromLocal,
+  reducers: {
+    addTodo: (state, { payload }) => {
+      state.todos = [...state.todos, payload];
+      todosSlice.caseReducers.calculateTotal(state);
+    },
+    removeTodo: (state, { payload }) => {
+      state.todos = state.todos.filter((todo) => todo.id !== payload);
+      todosSlice.caseReducers.calculateTotal(state);
+      toast.success("Deleted successfully");
+    },
+    changeStatusTodo: (state, { payload }) => {
+      const item = state.todos.find((todo) => todo.id == payload);
+      item.completed = !item.completed;
+      todosSlice.caseReducers.calculateTotal(state);
+    },
 
-export const {addTodo, removeTodo, changeStatusTodo} = todosSlice.actions;
+    calculateTotal: (state) => {
+      localStorage.setItem("todos", JSON.stringify(state));
+      let done = 0;
+      let notDone = 0;
+      state.todos.forEach((todo) => {
+        if (todo.completed) {
+          done += 1;
+        } else {
+            notDone +=1;
+        }
+      });
+      state.completedCount = done;
+      state.unCompletedCount = notDone;
+    },
+  },
+});
+
+export const { addTodo, removeTodo, changeStatusTodo, completedCount, unCompletedCount } = todosSlice.actions;
 
 export default todosSlice.reducer;
